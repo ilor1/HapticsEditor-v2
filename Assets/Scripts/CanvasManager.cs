e3930a3629f69cd1b6ece90f4a8c0a9277600119
ilor1 <riautermi@gmail.com>
2023-04-29 15:57:47 +0300

    Fix LineDrawer start and end

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UIElements;
@@ -8,14 +9,14 @@ public class CanvasManager : MonoBehaviour
 {
     public int TimeInMilliseconds;
 
-    public Vector2 Size = new Vector2(1920, 1080);
+    public float2 Size = new float2(1920, 1080);
     public int LengthInMilliseconds = 15000;
 
     public HapticScript[] HapticScripts;
     public UIDocument _UIDocument;
 
     private List<LineDrawer> _lineDrawers = new List<LineDrawer>();
-
+    private List<float2> _coords = new List<float2>();
 
     void Update()
     {
@@ -39,65 +40,104 @@ public class CanvasManager : MonoBehaviour
         {
             _lineDrawers[i].StrokeColor = HapticScripts[i].StrokeColor;
             _lineDrawers[i].LineWidth = HapticScripts[i].LineWidth;
-            _lineDrawers[i].Points = ConvertFunActionsToPoints(HapticScripts[i].Points);
+            _lineDrawers[i].Coords = ConvertActionsToCoords(HapticScripts[i].Points);
             _lineDrawers[i].MarkDirtyRepaint();
         }
     }
 
-    Vector2[] ConvertFunActionsToPoints(Vector2[] funactions)
+    float2[] ConvertActionsToCoords(int2[] actions)
     {
-        List<Vector2> coords = new List<Vector2>();
-        Array.Sort(funactions, new Vector2Comparer());
+        _coords.Clear();
+        Array.Sort(actions, new Int2Comparer());
 
         bool firstPoint = false;
         bool lastPoint = false;
+        float2 coord = float2.zero;
 
-        for (int i = 0; i < funactions.Length; i++)
+        for (int i = 0; i < actions.Length; i++)
         {
-            float at = funactions[i].x;
-            float pos = funactions[i].y;
-            float x = 0;
-            float y = 0;
+            float at = actions[i].x;
+            float pos = actions[i].y;
 
-            if (at < TimeInMilliseconds - 0.5f * LengthInMilliseconds) continue;
+            // Action.Pos is before timeline
+            if (at < TimeInMilliseconds - 0.5f * LengthInMilliseconds)
+            {
+                // if the last point is before the timeline start, draw a flat line
+                if (i == actions.Length - 1)
+                {
+                    coord.y = pos * -(Size.y / 100);
+                    coord.x = 0;
+                    _coords.Add(coord);
+                    
+                    coord.x = LengthInMilliseconds * (Size.x / LengthInMilliseconds);
+                    _coords.Add(coord);
+                }
+                
+                continue;
+            }
 
             // Get first point that is outside the screen
             if (!firstPoint && i > 0)
             {
                 firstPoint = true;
-                x = (funactions[i - 1].x - TimeInMilliseconds + LengthInMilliseconds * 0.5f) * (Size.x / LengthInMilliseconds);
-                y = funactions[i - 1].y * -(Size.y / 100);
-                coords.Add(new Vector2(x, y));
+
+                // Draw value at the start of the screen
+
+                int at0 = actions[i - 1].x;
+                
+                // if the first point is inside the timeline, we need to draw a separate coordinate at 0
+                if (at0 > TimeInMilliseconds - 0.5f * LengthInMilliseconds)
+                {
+                    coord.x = 0;
+                    coord.y = actions[i - 1].y * -(Size.y / 100);
+                    _coords.Add(coord);
+                }
+
+                coord.x = (actions[i - 1].x - TimeInMilliseconds + LengthInMilliseconds * 0.5f) * (Size.x / LengthInMilliseconds);
+                coord.y = actions[i - 1].y * -(Size.y / 100);
+                _coords.Add(coord);
             }
 
-            x = (at - TimeInMilliseconds + LengthInMilliseconds * 0.5f) * (Size.x / LengthInMilliseconds);
-            y = pos * -(Size.y / 100);
+            // Draw point
+            coord.x = (at - TimeInMilliseconds + LengthInMilliseconds * 0.5f) * (Size.x / LengthInMilliseconds);
+            coord.y = pos * -(Size.y / 100);
+            _coords.Add(coord);
 
-            //Debug.Log($"at:{at}, pos{pos}, coord({x},{y})");
-            coords.Add(new Vector2(x, y));
-            
-            
+            // Draw value at the end of the screen, when the last point is beyond timeline end
             if (at > TimeInMilliseconds + 0.5f * LengthInMilliseconds)
             {
+                float t = (TimeInMilliseconds + 0.5f * LengthInMilliseconds - actions[i - 1].x) / (actions[i].x - actions[i - 1].x);
+                coord.x = LengthInMilliseconds * (Size.x / LengthInMilliseconds);
+                coord.y = math.lerp(actions[i - 1].y, actions[i].y, t) * -(Size.y / 100);
+                _coords.Add(coord);
                 break;
+            }
+
+            // Draw value at the end of the screen, when the last point is inside timeline end
+            if (i == actions.Length - 1 && at < TimeInMilliseconds + 0.5f * LengthInMilliseconds)
+            {
+                // Add point to the end
+                coord.x = LengthInMilliseconds * (Size.x / LengthInMilliseconds);
+                coord.y = pos * -(Size.y / 100);
+                _coords.Add(coord);
             }
         }
 
-        return coords.ToArray();
+        return _coords.ToArray();
     }
 }
 
 [Serializable]
 public struct HapticScript
 {
-    public Vector2[] Points;
+    public int2[] Points;
     public Color StrokeColor;
     public float LineWidth;
 }
 
-public class Vector2Comparer : IComparer<Vector2>
+public class Int2Comparer : IComparer<int2>
 {
-    public int Compare(Vector2 a, Vector2 b)
+    public int Compare(int2 a, int2 b)
     {
         if (a.x < b.x)
         {
--- Assets/Scripts/LineDrawer.cs
+++ Assets/Scripts/LineDrawer.cs
@@ -1,11 +1,12 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class LineDrawer : VisualElement
 {
-    public Color StrokeColor;
-    public Vector2[] Points;
-    public float LineWidth;
+    public Color StrokeColor { get; set; }
+    public float2[] Coords { get; set; }
+    public float LineWidth { get; set; }
 
     public LineDrawer()
     {
@@ -14,25 +15,28 @@ public class LineDrawer : VisualElement
 
     private void OnGenerateVisualContent(MeshGenerationContext mgc)
     {
+        // TODO: Automatically split to multiple meshes if the line doesn't the vertex limit.
+        
         var paint2D = mgc.painter2D;
-        if (Points.Length <= 0)
+        if (Coords.Length <= 0)
         {
             return;
         }
-        
+
         // Settings
         paint2D.lineJoin = LineJoin.Round;
         paint2D.lineCap = LineCap.Butt;
         paint2D.strokeColor = StrokeColor;
         paint2D.lineWidth = LineWidth;
-     
+
         // Draw
         paint2D.BeginPath();
-        paint2D.MoveTo(Points[0]);
-        for (int i = 1; i < Points.Length; i++)
+        paint2D.MoveTo(Coords[0]);
+        for (int i = 1; i < Coords.Length; i++)
         {
-            paint2D.LineTo(Points[i]);
+            paint2D.LineTo(Coords[i]);
         }
+
         paint2D.Stroke();
     }
 }
